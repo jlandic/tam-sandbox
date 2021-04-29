09de3dfab5276bb0489ac339576e1aabaeb79571
Jonathan Landicheff <jlandicheff@kilig.io>
2021-04-30 00:05:22 +0200

    Beautify render

--- src/plugins/universe/plugin.rs
+++ src/plugins/universe/plugin.rs
@@ -17,12 +17,12 @@ fn spawn_galaxy(mut commands: Commands, seed: Res<Seed>) {
     let galaxy = Galaxy::generate(
         &seed,
         &GalaxySettings {
-            stars_count: 10_000,
+            stars_count: 100_000,
             gravity: 2.0,
             radius: 800.0,
             arms_count: 8,
             arm_spread: 0.1,
-            rotation_strength: 0.8,
+            rotation_strength: 8.0,
         },
     );
 
